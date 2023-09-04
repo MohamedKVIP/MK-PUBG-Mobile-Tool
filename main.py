faea9f01df1097ef7f4ee45d37a8f9a4cf63be32
Mohamed Kamal <m.k22353@gmail.com>
2023-09-04 04:54:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from src.update import UpdateWindow
 
 APP_NAME = "MK PUBG Mobile Tool"
 APP_VERSION = "v1.0.4"
-ctypes.windll.kernel32.SetConsoleTitleW(f"{APP_NAME} {APP_VERSION}")
+FULL_APP_NAME = f"{APP_NAME} {APP_VERSION}"
+ctypes.windll.kernel32.SetConsoleTitleW(FULL_APP_NAME)
 
 
 def hide_command_prompt():
@@ -21,32 +22,39 @@ def hide_command_prompt():
     Hide the command prompt window when launching the GUI app.
     """
     _apps = [
-        "python.exe",
-        "pythonw.exe",
-        "cmd.exe",
-        "powershell.exe",
+        "python",
+        "pythonw",
+        "cmd",
+        "powershell",
         "terminal"
     ]
     first_iteration = True
+    try_num = 0
     while True:
         active_window = win32gui.GetForegroundWindow()
         window_title = str(win32gui.GetWindowText(active_window)).lower()
         thread_id, current_pid = win32process.GetWindowThreadProcessId(active_window)
         current_target_name = psutil.Process(current_pid).name().lower()
 
-        app_name_lower = APP_NAME.lower()
-        window_title_lower = window_title.lower() if window_title else ""
+        full_app_name_lower = FULL_APP_NAME.lower()
+        window_title_lower = window_title.lower()
 
-        if any(
-                forbidden_app in current_target_name for forbidden_app in
-                _apps) and app_name_lower in window_title_lower:
+        if (any(forbidden_app in current_target_name for forbidden_app in _apps) and
+            full_app_name_lower in window_title_lower) or (
+            full_app_name_lower in window_title_lower and "explorer" not in current_target_name):
             ctypes.windll.user32.ShowWindow(active_window, 0)
             break
 
         if first_iteration:
             print("[!] Click on the command prompt window to hide it and start the GUI app")
             first_iteration = False
+        try_num += 1
         time.sleep(0.5)
+        if try_num > 30:
+            print(f"Please send me the following message: \n"
+                  f"window_title: {window_title}\n"
+                  f"current_target_name: {current_target_name}")
+            break
 
 
 def run_application():
